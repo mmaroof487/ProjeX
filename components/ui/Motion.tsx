cb6b57324632ae40131ee1c3d71fde0ac21f38c2
Mohammad Maroof <mmaroof487@gmail.com>
2025-10-22 00:39:55 +0530

    feat: added animation when rendering

--- components/ui/Motion.tsx
+++ components/ui/Motion.tsx
@@ -1,26 +1,6 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
-import Image from "next/image";
-
-export const Logo = () => {
-	return (
-		<motion.div initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1 }}>
-			<Link href="/">
-				<Image src="/logo.png" alt="Logo" width={144} height={30} className="w-auto h-auto" />
-			</Link>
-		</motion.div>
-	);
-};
-
-export const Label = ({ query }: { query: string }) => {
-	return (
-		<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5 }}>
-			<p className="text-30-semibold">{query}</p>
-		</motion.div>
-	);
-};
 
 export const Animate = ({ children, ix, iy, d }: { children: React.ReactNode; ix?: number; iy?: number; ox?: number; oy?: number; d?: number }) => {
 	return (
